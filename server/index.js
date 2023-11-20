7f5d13199738655a8158688fd54c9727ea2af45d
is-paranoia <55973519+is-paranoia@users.noreply.github.com>
2023-11-21 03:38:40 +0500

    add docker-compose

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
+import 'dotenv/config';
 import express from "express";
 import cors from "cors";
+import redis from 'redis';
+
+const REDIS = redis.createClient({
+    socket: {
+        host: 'redis',
+        port: process.env.REDIS_PORT
+    }
+});
+REDIS.on('error', (err) => console.log('Redis Client Error', err));
+await REDIS.connect();
 
 const app = express();
-//const cors = require("cors")
-const port = 8080;
+const port = process.env.MAIN_SERVER_PORT;
 
 app.use(express.json());
 app.use(cors())
 
 app.post("/blog", (req, res) => {
-//create new blog post
+    //create new blog post
 });
 
 app.get("/", (req, res) => {
@@ -18,8 +28,21 @@ app.get("/", (req, res) => {
 
 app.get("/pokemons", async (req, res) => {
     const {offset, limit} = req.query
+    const pokemonsCacheKey = `pokemons-${offset}-${limit}`
 
-    let pokemonsWithNames = [];
+    try {
+        console.log('req', req.query); // тут запилить по пользователю
+        const cachedPokemons = JSON.parse(await REDIS.get(pokemonsCacheKey));
+        await REDIS.expire(pokemonsCacheKey, 60)
+
+        if (cachedPokemons) {
+            console.log('USED FROM CACHE');
+            res.send(cachedPokemons);
+            return;
+        }
+    } catch {
+        console.log('Redis cache error!');
+    }
 
     try {
         const pokemonsRaw = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
@@ -38,8 +61,29 @@ app.get("/pokemons", async (req, res) => {
             return {...pokemon, id, height, abilities, image}
         }))
 
-        console.log('pokemonsFilled', pokemonsFilled[0]);
+        //console.log('pokemonsFilled', pokemonsFilled[0]);
+
+        if (pokemonsFilled) {
+            console.log('CREATE cachedPokemons');
+            await REDIS.set(
+                pokemonsCacheKey, 
+                JSON.stringify(pokemonsFilled),
+                'EX', 60
+            );
+            await REDIS.expire(pokemonsCacheKey, 60)
+        }
         res.send(pokemonsFilled)
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+app.get("/last", async (req, res) => {
+    try {
+        console.log('chzh');
+        const checkRedis = await REDIS.get('last');
+        console.log(JSON.parse(checkRedis));
+        res.send(JSON.parse(checkRedis))
     } catch {
         res.sendStatus(500);
     }
@@ -47,4 +91,5 @@ app.get("/pokemons", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
+  console.log(`Redis Port ${process.env.REDIS_PORT}!`);
 });
